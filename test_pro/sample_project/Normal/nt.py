00fb49329d5760a577b60a8378ad11a9291dc13d
piyushdravyakar46 <piyushdravyakar46@gmail.com>
2022-09-21 18:14:42 +0530

    t

--- test_pro/sample_project/Normal/nt.py
+++ test_pro/sample_project/Normal/nt.py
@@ -9,20 +9,8 @@ driver = webdriver.Chrome(service=object)
 
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 
-#radio Buttons
-# radio = driver.find_elements(By.NAME, "radioButton")
-#
-# def test_radioBtn():
-#     print("Radio button is exicute one by one")
-#     for btn in radio:
-#             print("!!!!!!!!!!!!!!!!!!")
-#             time.sleep(1)
-#             btn.click()
 
-
-#suggesion drop down with using implisitly wait and explicitly wait
-
-driver.find_element(By.CSS_SELECTOR, "#autocomplete").send_keys("ind")
+driver.find_element(By.CSS_SELECTOR, "#autocomplete").send_keys("in")
 driver.implicitly_wait(10)
 
 contry = driver.find_elements(By.XPATH, "//li[@class='ui-menu-item']")
@@ -31,22 +19,3 @@ for con in contry:
     name = con.find_element(By.XPATH, "div").text
     if name == "India":
         con.click()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
